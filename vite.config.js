f16596e47552656355f3732c9497ce0e9fb74b45
KenLesro <kenlesrolau@gmail.com>
2025-12-05 01:23:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",  // <--- 这一行是救命稻草，必须加！
 })
